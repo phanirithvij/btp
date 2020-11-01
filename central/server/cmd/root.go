0e1f7a8207d816297e7b20a3433e11ea8241e62c
phanirithvij <phanirithvij2000@gmail.com>
2020-11-01 23:25:16 +0530

    todo check completions after reboot

--- central/server/cmd/root.go
+++ central/server/cmd/root.go
@@ -14,8 +14,8 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use: "server",
-	// Use:   ExecName(),
+	// Use: "server",
+	Use:   ExecName(),
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
